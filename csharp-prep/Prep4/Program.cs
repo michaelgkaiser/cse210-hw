1e2482a95f9cbd78f025292656b8c11b9d2bb084
michaelgkaiser <mgkaiser79@gmail.com>
2024-03-03 23:34:54 -0700

    Finished Prep4 with help from answer key

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int>numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        
+        int sum = 0;
+
+        foreach (int input in numbers)
+        {
+            sum += input;
+        }
+        Console.WriteLine($"The sum of all numbers is: {sum}");
+
+        float average = ((float)sum) / numbers.Count();
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers.Max();
+        Console.WriteLine($"The highest number is: {max}");
     }
 }
